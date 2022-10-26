ae2ee8cfa29b7232e24cbd71d20c32375f5f5409
incuso <numismatica@gmail.com>
2022-10-26 09:56:38 +0200

    spostamento esempio

--- 1160-puntaritm.c
+++ 1160-puntaritm.c
@@ -1,36 +1,27 @@
-// aritmetica dei puntatori
+// uso operatori [] ed equivalenza aritmetica dei puntatori
 #include<stdio.h>
 #include<stdlib.h>
 
 int main(int argc, char **argv){
 
-  int a[8]={3, 4, 5, 6, 77, 88, 99, 34};
-  int *fp;
+  int a[]={11, 22, 33 ,44 ,55 ,66 ,77 ,88, 99};
 
-  fp=a;
+  printf("a[4]   vale %d\n", a[4]);
+  printf("*(a+4) vale %d\n", *(a+4));
+  printf("4[a]   vale %d\n", 4[a]); // verra' segnato come errore in sede di esame
 
-  printf("Ora fp vale %p, e ha dimensione %zd mentre a[] ha dimensione %zd\n", fp, sizeof(fp), sizeof(a));
-
-  for(int i=0; i<8; ++i)
-  {
-    fp = fp + 1; // NON mi sposto di un byte ma del numero di byte occupati da un int (4 nel nostro caso)
-    printf("All'indirizzo %p trovo %d\n", fp, *fp);
-  }
 
   return 0;
 }
 
-/* esempio esecuzione
+/*
 
-Ora fp vale 0x7fff7dde7200, e ha dimensione 8 mentre a[] ha dimensione 32
-All'indirizzo 0x7fff7dde7200 trovo 3
-All'indirizzo 0x7fff7dde7204 trovo 4
-All'indirizzo 0x7fff7dde7208 trovo 5
-All'indirizzo 0x7fff7dde720c trovo 6
-All'indirizzo 0x7fff7dde7210 trovo 77
-All'indirizzo 0x7fff7dde7214 trovo 88
-All'indirizzo 0x7fff7dde7218 trovo 99
-All'indirizzo 0x7fff7dde721c trovo 34
+   Esempio esecuzione 
+
+   a[4]   vale 55
+   *(a+4) vale 55
+   4[a]   vale 55
 
 */
+
 
