1069ac262d704a4c4970019fb76cf660433ef5b9
incuso <numismatica@gmail.com>
2022-10-26 16:37:50 +0200

    rimosso il 100

--- 1110-vla.c
+++ 1110-vla.c
@@ -16,32 +16,28 @@ int main(int argc, char **argv){
   // non rischio buffer overrun
 
 
-  if(n<=100)
+  // li leggo da tastiera
+  for(int i=0; i<n; ++i) 
   {
+    printf("Inserisci il numero intero #%d: ", i+1);
+    scanf("%d", &numbers[i]); // per leggere con scanf() uso la & come per una variabile "normale"
+  }
 
-    // li leggo da tastiera
-    for(int i=0; i<n; ++i) 
-    {
-      printf("Inserisci il numero intero #%d: ", i+1);
-      scanf("%d", &numbers[i]); // per leggere con scanf() uso la & come per una variabile "normale"
-    }
-
-    // li stampo
-    printf("I numeri inseriti sono: ");
-    for(int i=0; i<n; ++i)
-    {
-      printf("%d ", numbers[i]); 
-    }
+  // li stampo
+  printf("I numeri inseriti sono: ");
+  for(int i=0; i<n; ++i)
+  {
+    printf("%d ", numbers[i]); 
+  }
 
 
-    // calcolo la somma (di fatto potevo farlo in uno dei cicli precedenti)
-    int sum = 0;
-    for(int i=0; i<n; ++i)
-      sum += numbers[i];
+  // calcolo la somma (di fatto potevo farlo in uno dei cicli precedenti)
+  int sum = 0;
+  for(int i=0; i<n; ++i)
+    sum += numbers[i];
 
-    printf("\nla loro somma e': %d\n", sum);
+  printf("\nla loro somma e': %d\n", sum);
 
-  } // if(n<=100)
 
 
 
