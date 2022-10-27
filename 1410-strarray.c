ea149c66de880a7e251c7b3a263e98bebf3cb057
incuso <numismatica@gmail.com>
2022-10-27 10:52:57 +0200

    renumbering

--- 1410-strarray.c
+++ 1410-strarray.c
@@ -1,4 +1,4 @@
-// array di stringhe mediante array di puntatori
+// array di stringhe mediante array bidimensionale
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -17,21 +17,19 @@ che nel pensier rinova la paura!";
   char *word;
 
 
-  // un array di puntatori puo' essere usato per memorizzare tante stringhe
+  // un array bidimensionale puo' essere usato per memorizzare tante stringhe
   // devo:
-  // 1. dimensionare il numero massimo di stringhe da gestire
+  // 1. dimensionare quante stringhe devo gestire
+  // 2. dimensionare la massima lunghezza che devo gestire per una stringa
 
-  char *str_array[100];   // con questo array posso memorizzare fino a 100 stringhe, attenzione che non c'e' limite alla lunghezza di ciascuna stringa (a parte i limiti di memoria) ma devo comunque allocarli
-  int mem_used=sizeof(str_array);
+  char str_array[100][25];   // con questo array posso memorizzare fino a 100 strimghe di massimo 24 caratteri
 
   int i;
   word = strtok(incipit, delimitatori);
   for(i=0; word && i<100; ++i) // mi fermo se finisco con strtok o se raggiungo massimo numero di stringhe
   {
     // NUOVO ELEMENTO: nel caso di array bidimensionali se ometto il secondo indice ho l'indirizzo della riga
-    str_array[i] = malloc(sizeof(char) * (strlen(word) + 1)); // alloco spazio necessario in base a quanto devo memorizzare
     strcpy(str_array[i], word); // man mano che ho una parola la ricopio nella posizione giusta nell'array
-    mem_used += sizeof(char) * (strlen(word) + 1);
 
     word = strtok(NULL, delimitatori);
   }
@@ -39,10 +37,9 @@ che nel pensier rinova la paura!";
   for(int j; j<i; ++j)
   { 
     printf("#%d \t %s\n", j, str_array[j]);
-    free(str_array[j]); // libero memoria visto che non la uso piu'
   }
 
-  printf("Per memorizzare le stringhe ho usato %d byte di memoria\n", mem_used);
+  printf("Per memorizzare le stringhe ho usato %zd byte di memoria\n", sizeof(str_array));
 
   return 0;
 }
