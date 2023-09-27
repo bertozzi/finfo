5925c4d3cf5a14acaca3c598819ac99d87d94f3c
Massimo <bertozzi@ce.unipr.it>
2023-09-27 11:31:50 +0200

    corretto variabile

--- 0120-identificativi.c
+++ 0120-identificativi.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv){
 
   //float char; // se scommentato da' errore -> non posso usare parola riservata del C
 
-  //long 99luftballons; // se scommentato da' errore -> gli identificativi devono sempre iniziare per lettera
+  //int 99luftballons; // se scommentato da' errore -> gli identificativi devono sempre iniziare per lettera
 
   double class;  // compila, class non e' parola riservata del C. Lo e' del C++ e quindi non e' una grande idea usarla...
 
