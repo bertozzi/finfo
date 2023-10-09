5728f836c4640f669a6aaa1dba3949fddac8d6d3
incuso <numismatica@gmail.com>
2023-10-09 10:18:41 +0200

    rimodulati esercizi espressioni

--- 0370-bitwise.c
+++ 0370-bitwise.c
@@ -1,36 +1,29 @@
-// esempi di operazioni bit a bit
+// esempi di operazioni bit a bit, stampa di un numero in formato binario
 #include<stdio.h>
 #include<stdlib.h>
 
 int main(int argc, char **argv){
 
   int a;
-  printf("Inserisci un numero intero: ");
+  printf("Inserisci un numero intero < 256: ");
   scanf("%d", &a);
 
 
-  printf("Il numero inserito vale: %d (%08x)\n", a, a);
+  printf("Il numero inserito e' %d e in binario vale ", a);
 
-  int c1 = ~a; // nego bit a bit, quindi gli 1->0 e gli 0->1 e' un complemento a tutti gli effetti
+  printf("%d", (a>>7)&1 ); // in questo caso sposto l'ottavo bit nella posizione 0 ovvero come 'bit meno significativo'. Tramite l'and bit a bit tengo solo lui
+  printf("%d", (a>>6)&1 );
+  printf("%d", (a>>5)&1 );
+  printf("%d", (a>>4)&1 );
+  printf("%d", (a>>3)&1 );
+  printf("%d", (a>>2)&1 );
+  printf("%d", (a>>1)&1 );
+  printf("%d", (a>>0)&1 );
 
-  printf("Il suo complemento a uno vale: %d (%08x)\n", c1, c1);
-  
-  int c2 = ~a + 1;  // come il precedente ma aggiungo 1 -> complemento a 2
+  // piu' avanti scopriremo quanto questo codice e' brutto, ma per ora non abbiamo parlato di cicli...
 
-  printf("Il suo complemento a due vale: %d (%08x)\n", c2, c2);
+  printf("\n");
 
-
-  int b;
-  printf("Quale bit vuoi esaminare? ");
-  scanf("%d", &b);
-
-  int bit = (a & (1 << b)) >> b ; 
-  // tramite (1<<b) creo un numero che dal punto di vista binario ha un solo '1' nella posizione 'b'.a
-  // con l'and bit a bit isolo il bit corrispondente di a
-  // l'ulteriore shift lo riporta nella posizione 'bit meno significativo'
-  // Semplificando potevo scrivere ((a >> b) & 1)
-
-  printf("Il bit %d di %d vale %d\n", b, a, bit); 
 
   return 0;
 }
