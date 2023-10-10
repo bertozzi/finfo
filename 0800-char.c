5492fe2cee2e9ac037b5b7779e0aee330551b59b
incuso <numismatica@gmail.com>
2023-10-10 15:32:02 +0200

    un po' di rinumerazioni incasinate

--- 0800-char.c
+++ 0800-char.c
@@ -5,12 +5,16 @@
 int main(int argc, char **argv){
 
   char c;
+  unsigned int code;
 
-  printf("Inserisci un simbolo da tastiera: ");
-  scanf("%c", &c);
+  printf("Inserisci il codice ASCII di un carattere: ");
+  scanf("%u", &code);
 
-  printf("Il simbolo [%c] ha codice ASCII %d\n", c, c);
- 
+  if(code<=255)
+  {
+    c=code;
+    printf("Il codice ASCII %u corrisponde al carattere \"%c\"\n", code, c);
+  }
 
 
   return 0;
--- 0820-chario.c
+++ 0820-chario.c
@@ -1,28 +1,20 @@
-// lettura da tastiera di simboli, accortezze...
+// buffer di tastiera
 #include<stdio.h>
 #include<stdlib.h>
 
 int main(int argc, char **argv){
 
-  int a, b;
-  char c, d;
 
-  printf("Inserisci un numero da tastiera: ");
-  scanf("%d", &a);
+  char a;
 
-  printf("Inserisci un secondo numero da tastiera: ");
-  scanf("%d", &b); // PROBLEMA: quando premo i tasti i relativi simboli finiscono in input al programma. Tutti. Incluso l'invio dopo il numero...
+  printf("Inserisci una frase: ");
 
-  printf("I numeri sono %d & %d\n", a, b);
-
- 
-  printf("Inserisci un simbolo da tastiera: ");
-  scanf("%c", &c);  // gia' qui non funziona! cosa e' successo?
-
-  printf("Inserisci un secondo simbolo da tastiera: ");
-  scanf("%c", &d);
-
-  printf("I simboli inseriti sono %c & %c\n", c, d);
+  do
+  {
+    scanf("%c", &a);
+    printf("Il codice ASCII di [%c] e' %d\n", a, a);
+  }
+  while(a != '\n'); // si', le sequenze di escape si possono usare anche come caratteri singoli e non solo nelle stringhe di formato
 
   return 0;
 }
