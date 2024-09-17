ff03d1c50d60c78f9857995850fd8ab5cc2b12eb
incuso <numismatica@gmail.com>
2024-09-17 16:10:36 +0200

    some comment

--- 0080-commenti.c
+++ 0080-commenti.c
@@ -1,16 +1,18 @@
 // introduzione ai commenti
 
-// Questo e' un commento su singola linea, il compilatore ignora qualunque elemento dopo //
+// Questo e' un commento su singola linea, il compilatore ignora qualunque elemento dopo due 'slash' //
 #include <stdio.h>
 
 int main// un commento puo' cominciare in qualsiasi posizione
 // ma termina alla fine della riga
-( // la parentesi chiusa alla fine di questa riga fa parte del commento)
+// quindi il compilatore ignora tutto quanto trova da // fino alla fine della riga
+( // la parentesi chiusa alla fine di questa riga di conseguenza e' parte del commento)
   )
 
 {  // comincia il corpo del programma
   printf("Commentiamo!\n");
-  return 0;}
+  return 0;
+}
 
   // questo commento si trova dopo la fine del programma
 
@@ -19,6 +21,8 @@ int main// un commento puo' cominciare in qualsiasi posizione
      Posso scrivere qualunque cosa qui dentro
 
      Termina solo quando incontro questo  */
+
+  // come sopra il compilatore ignora tutto quanto trova a partire da /* fino a quando non incontrera' un */
 
 
 
