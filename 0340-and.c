9ffa09d85c0588c62d154d0494bf7fc755d1561b
incuso <numismatica@gmail.com>
2024-10-03 17:23:59 +0200

    small improvements

--- 0340-and.c
+++ 0340-and.c
@@ -6,18 +6,34 @@
 int main(int argc, char **argv){
   int n, d;
 
-  printf("Introduci due interi positivi: ");
+  printf("Please, enter two positive integer numbers: ");
   scanf("%d", &n);
   scanf("%d", &d);
 
-  int risultato = (d > 0 && n%d == 0); // prima viene stimato d > 0, se e solo se vera si stima anche n%d == 0
-  // perche'? Ma perche' se stimassi n%d senza controllare il valore di d, rischio una divisione per 0
-  // il cortocircuto avviene in quanto ho un AND logico (&&). In questo caso basta che una delle due componenti
-  // dell'and sia falsa per far si' che tutta l'espressione sia falsa. Se d>0 e' falsxa diventa del tutto
-  // inutile valutare anche l'altra sottoespressione
-  
-  printf("Il risultato dell'espressione (%d > 0 && %d%%%d == 0) e' %d\n", d, n, d, risultato);
-       
+  int risultato = (d != 0 && n%d == 0); 
+  printf("Il risultato dell'espressione (%d != 0 && %d%%%d == 0) e' %d\n", d, n, d, risultato);
+
+  // lets try the same expression simply inverting the operands
+  // formally this should be the same as previous one 
+  risultato = (n%d == 0 && d != 0);
+  printf("Il risultato dell'espressione ( %d%%%d == 0 && %d != 0) e' %d\n", d, n, d, risultato);
+
+  /* FIXME
+     when we input 0 for d the program fails during execution. Why?
+     When d == 0, n%d can not be computed sine it implies a division by 0
+     But in both conditions we have n%d, and therefore why the evaluation
+     of the first expression does not fail?
+     Anyway, when logical operators are used, shortcuts are applied.
+     The evaluation of && stops when the first operand is false, since
+     this implies that the whole expression is obviously false.
+     Therefore the evaluation of (d != 0 && n%d == 0) stops when d == 0 
+     without even bothering to compute n%d == 0
+     This is not happening when the second expression is evaluated.
+     In such a case, initially the system tries to evaluate n%d == 0
+     therefore failing...
+
+   */
+
 
 
   return 0;
