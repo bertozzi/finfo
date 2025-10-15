255ff5fc1bfd5542bcc5da004d068a109f8a5d6d
Massimo <bertozzi@ce.unipr.it>
2025-10-15 10:12:04 +0200

    some comments

--- 0760-continue.c
+++ 0760-continue.c
@@ -11,7 +11,12 @@ int main (int argv, char **argc) {
   {
     scanf("%d",&n);
 
-    if(n<=0 || n>255) continue; // i codici ascii estesi sono tra 0 e 255
+    // i codici ascii estesi sono tra 0 e 255 se viene inserito un valore al di fuori di questo
+    // intervallo non devo proseguire con l'esecuzione 
+    // soluzione: uso lo statement "continue" che salta subito
+    // alla valutazione della condizione di ciclo nel caso di while() e do-while()
+    // o all'aggiornamento per il for()
+    if(n<=0 || n>255) continue;
 
     if(n>32)
       printf("Hai inserito un codice ASCII valido e stampabile che corrisponde al carattere: %c\n", n);
