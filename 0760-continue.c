a9553442e2ab3543f9cc7644f3869ed22123cb47
Massimo <bertozzi@ce.unipr.it>
2025-10-15 12:48:01 +0200

    mods

--- 0760-continue.c
+++ 0760-continue.c
@@ -16,14 +16,14 @@ int main (int argv, char **argc) {
     // soluzione: uso lo statement "continue" che salta subito
     // alla valutazione della condizione di ciclo nel caso di while() e do-while()
     // o all'aggiornamento per il for()
-    if(n<=0 || n>255) continue;
+    if(n <= 0 || n > 255) continue;
 
-    if(n>32)
+    if(n >= 32)
       printf("Hai inserito un codice ASCII valido e stampabile che corrisponde al carattere: %c\n", n);
     else 
       printf("Hai inserito un codice ASCII che corrisponde ad un carattere di controllo\n");
   }
-  while(n!=0);
+  while(n != 0);
 
    return 0;
 }
