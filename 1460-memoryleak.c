39c61a9c61a8f68c6d58f10f22e85b6337640009
incuso <numismatica@gmail.com>
2025-11-05 15:44:21 +0100

    better memory leak

--- 1460-memoryleak.c
+++ 1460-memoryleak.c
@@ -3,49 +3,51 @@
 #include<stdlib.h>
 
 
-int legginumerierestituiscisomma(unsigned int quanti)
-{
-  int *myarr = malloc(sizeof(int)*quanti); // alloco array per memorizzare quanto leggero' da tastiera
-  if(!myarr)
-  {
-    printf("Non sono riuscito ad allocare quanto richiesto! Esco...\n");
-    exit(1); // termina il programma
-  }
-  
-  printf("Solo per debug ti dico che hai allocato il buffer all'indirizzo %p\n", myarr);
-
-  // prima li leggo
-  for(int i=0; i<quanti; ++i)
-  {
-    printf("Inserisci numero intero: ");
-    scanf("%d", &myarr[i]);
-  }
-
-  // poi li sommo (potevo fare senza memorizzarli ma questo e' un esempio)
-  int sum = 0;
-  for(int i=0; i<quanti; ++i)
-    sum += myarr[i];
-
-  return sum; // termina la funzione. A parte ritornare dove era stata chiamata restituendo il valore contenuto in sum, vengono DISTRUTTE le varaibili locali: myarr, sum e quanti.
-  // e quanto avevo allocato? che fine fa?
-}
-
-
 int main(int argc, char **argv){
 
   unsigned int n;
+  int *myarr;      // per gestire array di interi allocato dinamicamente
+  int sum;
 
-  printf("Quanti interi vuoi sommare? ");
-  scanf("%u", &n);
+  while(1)
+  {
+    printf("Quanti interi vuoi sommare (0 per terminare)? ");
+    scanf("%u", &n);
 
-  int sum = legginumerierestituiscisomma(n);
-  printf("Il totale vale: %d\n", sum);
-    
-  printf("Dai, mi sto divertendo sommiamone altri, dimmi quanti: ");
-  scanf("%u", &n);
+    if(!n)
+      break;
 
-  sum = legginumerierestituiscisomma(n);
-  printf("Il nuovo totale vale: %d\n", sum);
+    myarr = malloc(sizeof(int) * n); // alloco array per memorizzare quanto leggero' da tastiera
+    if(!myarr)
+    {
+      printf("Non sono riuscito ad allocare quanto richiesto! Esco...\n");
+      exit(1); // termina il programma
+    }
+
+    printf("Solo per debug ti dico che hai allocato il buffer all'indirizzo %p\n", myarr);
+
+    // prima li leggo
+    for(int i = 0; i < n; ++i)
+    {
+      printf("Inserisci numero intero: ");
+      scanf("%d", &myarr[i]);
+    }
+
+    // poi li sommo (potevo fare senza memorizzarli ma questo e' un esempio)
+    sum = 0;
+    for(int i = 0; i < n; ++i)
+      sum += myarr[i];
+
+    printf("Il totale vale: %d\n", sum);
+
+    // il codice funziona da un punto di vista pratico (o, per essere piu' precisi, funziona inizialmente)
+    // pero' ad ogni ciclo ripeto una malloc(). Ogni volta viene riservata un'area di memoria
+    // MAI piu' liberata. 
+    // Quando ripeto la malloc() l'indirizzo di questa area di memoria lo perdiamo
+    // Non solo non riusciamo piu' ad accedervi ma neanche la possiamo liberare
+    // MEMORY LEAK
+    // Ripetendo il tutto, io rischio di esaurire la memoria.
+  }
 
 
 
